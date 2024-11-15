b9e4ae599ec27af486bf39129559fbf8dc8a53b1
Giovanni_T <giovannitummino.15@gmail.com>
2024-11-15 17:38:34 +0100

    mise a jour

--- back-end/start/routes.ts
+++ back-end/start/routes.ts
@@ -19,6 +19,8 @@ router.get('/', async () => {
 })
 
 const AuthController = () => import('#controllers/auth_controller')
+const ParcelsController = () => import('#controllers/parcels_controller')
+
 // User Management APIs
 router.post('/users/register', [AuthController, 'register']).as('auth.register')
 router.post('/users/login', [AuthController, 'login']).as('auth.login')
@@ -27,3 +29,11 @@ router.put('/users/:id', [AuthController, 'updateUser']).as('users.update').use(
 router.delete('/users/:id', [AuthController, 'deactivateUser']).as('users.deactivate').use(middleware.auth())
 router.post('/users/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
 router.get('/users/me', [AuthController, 'me']).as('auth.me').use(middleware.auth())
+
+// Parcel Management APIs
+router.post('/parcels/register', [ParcelsController, 'register']).as('parcels.register').use(middleware.auth())
+router.get('/parcels/:id', [ParcelsController, 'show']).as('parcels.show').use(middleware.auth())
+router.get('/parcels/available', [ParcelsController, 'available']).as('parcels.available').use(middleware.auth())
+router.put('/parcels/:id', [ParcelsController, 'update']).as('parcels.update').use(middleware.auth())
+router.post('/parcels/split', [ParcelsController, 'split']).as('parcels.split').use(middleware.auth())
+router.post('/parcels/merge', [ParcelsController, 'merge']).as('parcels.merge').use(middleware.auth())
